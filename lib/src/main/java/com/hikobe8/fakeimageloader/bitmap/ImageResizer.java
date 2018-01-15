d971a6d8f23a2e9aa7967aedc63392f1fe3947e7
hikobe8 <hikoeb8@163.com>
2018-01-16 00:11:50 +0800

    init

---
+++ lib/src/main/java/com/hikobe8/fakeimageloader/bitmap/ImageResizer.java
@@ -0,0 +1,8 @@
+package com.hikobe8.fakeimageloader.bitmap;
+
+/**
+ * Created by Ray on 2018/1/16.
+ */
+
+public class ImageResizer {
+}
---
+++ sample/src/main/java/com/hikobe8/fakerimageloader/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hikobe8.fakerimageloader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
